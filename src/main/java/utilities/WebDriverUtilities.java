9ccf06e617cd48d91fb382bc3e9360c106cee932
ManojCM26 <manojcm2611@gmail.com>
2025-07-21 13:15:16 +0530

    initials

---
+++ src/main/java/utilities/WebDriverUtilities.java
@@ -0,0 +1,11 @@
+package utilities;
+
+public class WebDriverUtilities {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WebDriver driver");
+		System.out.println("driver is from WebDriver interface");
+	}
+
+}
---
+++ src/test/java/organization/OrgTests.java
@@ -0,0 +1,8 @@
+package organization;
+
+public class OrgTests {
+	public static void main(String[] args) {
+		System.out.println("orgnization KLR created");
+		System.out.println("KLR works with cricket academies");
+	}
+}
